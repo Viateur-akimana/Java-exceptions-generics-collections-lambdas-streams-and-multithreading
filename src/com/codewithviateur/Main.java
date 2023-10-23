ae81d878371a2e26ab79c2d047d5a838824cd6da
Viateur <akimanaviateur94@gmail.com>
2023-10-23 21:17:54 +0200

    Advanced java

--- src/com/codewithviateur/Main.java
+++ src/com/codewithviateur/Main.java
@@ -1,9 +1,11 @@
 package com.codewithviateur;
 
+import com.codewithviateur.exceptions.Account;
 import com.codewithviateur.exceptions.ExceptionsDemo;
 
 public class Main {
     public static void main(String[] args) {
         ExceptionsDemo.show();
+        Account.deposit(4);
     }
 }
--- src/com/codewithviateur/exceptions/Account.java
+++ src/com/codewithviateur/exceptions/Account.java
@@ -1,16 +1,9 @@
 package com.codewithviateur.exceptions;
 
 public class Account {
-  private float balance;
-
-  public void deposit(float value)  {
-    if (value <= 0)
+  public static void deposit(float amount){
+    if(amount <= 0)
       throw new IllegalArgumentException();
-  }
-
-  public void withdraw(float value) throws AccountException {
-    if (value > balance)
-      throw new AccountException(new InsufficientFundsException());
   }
 }
 
