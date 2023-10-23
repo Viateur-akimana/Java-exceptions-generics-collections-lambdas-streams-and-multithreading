e08a79099bd4ccfa5c958d093cd1b94db658f511
Viateur <akimanaviateur94@gmail.com>
2023-10-23 21:08:31 +0200

    Advanced java

--- src/com/codewithviateur/Main.java
+++ src/com/codewithviateur/Main.java
@@ -1,6 +1,9 @@
 package com.codewithviateur;
 
+import com.codewithviateur.exceptions.ExceptionsDemo;
+
 public class Main {
     public static void main(String[] args) {
+        ExceptionsDemo.show();
     }
 }
--- src/com/codewithviateur/exceptions/ExceptionsDemo.java
+++ src/com/codewithviateur/exceptions/ExceptionsDemo.java
@@ -1,13 +1,26 @@
 package com.codewithviateur.exceptions;
 
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExceptionsDemo {
-  public static void show() {
-    var account = new Account();
+  public static void show(){
+    FileReader reader = null;
     try {
-      account.withdraw(10);
-    } catch (AccountException e) {
-      var cause = e.getCause();
-      System.out.println(cause.getMessage());
+      reader = new FileReader("file.txt");
+      var value = reader.read();
+    }
+    catch (IOException e){
+      System.out.println("The file doesn't exist");
+    }
+    finally {
+      try {
+        if (reader != null)
+        reader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
