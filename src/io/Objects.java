c6f408d18e81eccf81d2059df1f78accd98c28b7
Viateur <akimanaviateur94@gmail.com>
2023-11-06 15:55:22 +0100

    working woth object

--- src/io/Objects.java
+++ src/io/Objects.java
@@ -1,26 +1,29 @@
 package io;
-import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
 public class Objects {
-        public static void main(String[] args) {
-            // The string you want to write to the file
-            String content = "districts.txt";
 
-            // Specify the file path where you want to write the content
-            String filePath = "output.txt";
-
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-                // Write the content to the file
-                writer.write(content);
-
-                System.out.println("Content has been written to the file successfully.");
-            } catch (IOException e) {
-                // Handle any potential IOException that might occur
-                System.err.println("Error writing to the file: " + e.getMessage());
+        public static void main(String[] args) throws IOException {
+            FileReader inputStream = null;
+            FileWriter outputStream = null;
+            try {
+                inputStream = new FileReader("input.txt");
+                outputStream = new FileWriter("cput.txt");
+                int c;
+                while ((c = inputStream.read()) != -1) {
+                    outputStream.write(c);
+                }
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (outputStream != null) {
+                    outputStream.close();
+                }
             }
         }
     }
+
 
 
